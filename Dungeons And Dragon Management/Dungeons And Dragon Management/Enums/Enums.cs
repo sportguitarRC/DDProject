b7fedceb591087f5e2f9a24acd1405abaacc17ae
unknown <1131296@INFO-C3003-18.pedagogie.cegep-chicoutimi.qc.ca>
2015-04-07 10:01:02 -0400

    Every Classes

---
+++ Dungeons And Dragon Management/Dungeons And Dragon Management/Enums/Enums.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dungeons_And_Dragon_Management
+{
+    static class Enums
+    {
+        enum EquipmentSlot
+        {
+            Weapon,
+            Armor,
+            Shield,
+            Misc
+        }
+    }
+}
---
+++ Dungeons And Dragon Management/Dungeons And Dragon Management/Player/Character/Attributes/Equipment/AmorClass/ArmorClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dungeons_And_Dragon_Management
+{
+    class ArmorClass
+    {
+    }
+}
