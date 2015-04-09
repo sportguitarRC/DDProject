cd05d76d7e96c3e147b33bebb45a0cfab9a39ade
unknown <1131296@INFO-C3003-19.pedagogie.cegep-chicoutimi.qc.ca>
2015-04-09 16:30:21 -0400

    Client

--- Dungeons And Dragon Management/Dungeons And Dragon Management/NetworkEngine.cs
+++ Dungeons And Dragon Management/Dungeons And Dragon Management/NetworkEngine.cs
@@ -2,10 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net.Sockets;
+using System.Net;
+using System.Windows.Forms;
 
 namespace Dungeons_And_Dragon_Management
 {
     class NetworkEngine
     {
+        Socket socketConnection;
+        SocketAsyncEventArgs SAEA;
+
+        public NetworkEngine(string ipAdress, int port)
+        {
+            socketConnection = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ipInfo = new IPEndPoint(IPAddress.Parse(ipAdress), port) ;
+            SAEA.RemoteEndPoint = ipInfo;
+            SAEA = new SocketAsyncEventArgs();
+            SAEA.Completed+=new EventHandler<SocketAsyncEventArgs>(SAEA_Completed);
+            socketConnection.ConnectAsync(SAEA);
+        }
+
+        private void SAEA_Completed(object sender, SocketAsyncEventArgs e)
+        {
+            if (e.LastOperation == SocketAsyncOperation.Connect)
+            {
+                MessageBox.Show("Yééé!");
+            }
+
+            if (e.LastOperation == SocketAsyncOperation.Receive)
+            {
+            }
+
+            if (e.LastOperation == SocketAsyncOperation.Send)
+            {
+            }
+        }
+
+        private void sendToServer(byte[] messageToSend)
+        {
+
+        }
     }
 }
